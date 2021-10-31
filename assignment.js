fa66888f19b0ecd5a2aa4eadffdf830b2d7bdd98
Shubham Aggarwal <aggy.decent@gmail.com>
2021-10-31 17:10:59 +0530

    fixed tables relation and added email notification

--- assignment.js
+++ assignment.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const nodemailer = require('nodemailer');
+
+//create transport 
+const transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'your_gmail_id',//Add your email id to test send email notification
+        pass: 'your_gmail_password',//Password of that email id
+    },
+});
 
 // Create connection
 const db = mysql.createConnection({
     host : 'localhost',
     user : 'me',
     password : 'secret',
-    database : 'blogposts'
+    database : 'blog'
 })
 
 //Connect
@@ -25,7 +35,7 @@ app.use(express.json());
 
 // Create DB
 app.get('/createdb', (req,res)=> {
-    let sql = 'CREATE DATABASE blogposts';
+    let sql = 'CREATE DATABASE blog';
     db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
@@ -35,43 +45,76 @@ app.get('/createdb', (req,res)=> {
 
 // Create tables inside DB
 app.get('/createtables', (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), description VARCHAR(255), authorId int, PRIMARY KEY (id))'
     let sql2 = 'CREATE TABLE authors(id int AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255), PRIMARY KEY (id))'
-    db.query(sql, (err, result) => {
+    db.query(sql2, (err, result) => {
         if(err) throw err;
         console.log(result);
     })
-    db.query(sql2, (err, result) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), description VARCHAR(255), authorId int, PRIMARY KEY (id), FOREIGN KEY (authorId) REFERENCES authors(id))'
+    db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
         res.send('Tables created...');
     })
+    
 });
 
 //To save a new blog post
 app.post('/post', (req, res) => {
     let post = req.body;
-    let sql = 'INSERT INTO posts SET ?';
-    //let sql2 = 
-    let query = db.query(sql, post, (err, result) => {
+    let sql = `SELECT id from authors WHERE name LIKE '${post.authorName}'`;
+    db.query(sql, (err,result) => {
         if(err) throw err;
-        console.log(result);
-        res.send('Post Added!');
+        delete post['authorName'];
+        post['authorId'] = result[0].id;
+        let sql2 = 'INSERT INTO posts SET ?';
+        let query = db.query(sql2, post, (err, result) => {
+            if(err) throw err;
+            else {
+                console.log(result);
+                let sql3 = `SELECT email from authors`;
+                console.log()
+                db.query(sql3,(err, result) => {
+                    if(err) throw err;
+                    else {
+                        let mailingList = result.map( item => {
+                            return item['email'];
+                        }).toString();
+                        const mailOptions = {
+                            from: 'your_gmail_id',//need to replace with your email id
+                            to: `${mailingList}`,
+                            subject: 'New Blog Post',
+                            html: 'A new blog post has just been added! Make sure to check it out.',
+                        };
+                        transport.sendMail(mailOptions, (error, info) => {
+                            if (error) {
+                                console.log(error);
+                            }
+                            console.log(`Message sent: ${info.response}`);
+                        });
+                    }
+                })
+                res.send('Post Added!');
+            }
+        })
     })
-
 })
 
 //Update a post by its id
 app.put('/post/update', (req, res) => {
     let postId = req.query.num;
-    let data = req.body;
-    console.log(data);
-    console.log(postId);
-    let sql = `UPDATE posts SET title = '${data.title}', description = '${data.description}', authorId = ${data.authorId} WHERE id = ${postId}`;
-    let query = db.query(sql, (err, result) => {
+    let post = req.body;
+    let sql = `SELECT id from authors WHERE name LIKE '${post.authorName}'`;
+    db.query(sql, (err,result) => {
+        if(err) throw err;
+        delete post['authorName'];
+        post['authorId'] = Number(JSON.stringify(result).replace( /\D+/g, ''));
+        let sql2 = `UPDATE posts SET title = '${post.title}', description = '${post.description}', authorId = ${post.authorId} WHERE id = ${postId}`;
+        let query = db.query(sql2, (err, result) => {
         if(err) throw err;
         console.log(result);
         res.send('Post updated!');
+        })
     })
 })
 
@@ -97,10 +140,14 @@ app.get('/posts/authorId', (req, res) => {
 //Delete post by id
 app.delete('/post/delete', (req, res) => {
     let postId = req.query.num;
+    console.log(postId);
     let sql = `DELETE from posts WHERE id = ${postId}`;
     db.query(sql, (err,result) => {
         if(err) throw err;
-        console.log(result);
+        else {
+            console.log(result);
+            res.send('Post deleted!');
+        }
     })
 })
 
--- readme.txt
+++ readme.txt
@@ -1,47 +1,18 @@
-I created a database name 'blogposts', with tables posts and authors.
+I created a database name 'blog', with tables posts and authors.
 
-URL to create db - localhost:3000/createdb
-URL to create Tables - localhost:3000/createtables
-
-ADD POSTS INTO THE DATABASE
-URL - localhost:3000/post
-{
-    "title": "Post one",
-    "description": "I am the first blog post!",
-    "authorId": 1
-}
-
-{
-    "title": "Post two",
-    "description": "I am the second blog post!",
-    "authorId": 1
-}
-
-{
-    "title": "Post Third",
-    "description": "I am the third blog post!",
-    "authorId": 3
-}
-
-{
-    "title": "Post fourth",
-    "description": "I am the fourth blog post!",
-    "authorId": 2
-}
-
-{
-    "title": "Post fifth",
-    "description": "I am the fifth blog post!",
-    "authorId": 2
-}
-
+(before running this comment the 20th code line in assignment.js)
+URL to create db - localhost:3000/createdb 
+(Un comment the 20th line and re-run the file)
+URL to create Tables - localhost:3000/createtables 
 
 ADD AUTHORS INTO THE DATABASE
+*add your email and password on line 10,11 & line 84 in assignment.js*
+*Add real email ids for the email notification
 URL - localhost:3000/author
-{
     "name": "Michael Clark",
     "email": "mike95@yahoo.com"
-}
+}{
+
 {
     "name": "Jaqulin Goodman",
     "email": "jaqulin_goodman@gmail.com"
@@ -51,17 +22,48 @@ URL - localhost:3000/author
     "email": "author_Kdesai@gmai.com"
 }
 
+ADD POSTS INTO THE DATABASE
+URL - localhost:3000/post
+{
+    "title": "Post one",
+    "description": "I am the first blog post!",
+    "authorName": "Michael Clark"
+}
+
+{
+    "title": "Post two",
+    "description": "I am the second blog post!",
+    "authorName": "Michael Clark"
+}
+
+{
+    "title": "Post Third",
+    "description": "I am the third blog post!",
+    "authorName": "Shubhash Kumar Desai"
+}
+
+{
+    "title": "Post fourth",
+    "description": "I am the fourth blog post!",
+    "authorName": "Jaqulin Goodman"
+}
+
+{
+    "title": "Post fifth",
+    "description": "I am the fifth blog post!",
+    "authorName": "Jaqulin Goodman"
+}
+
 DELETE POST BY ID
-URL - localhost:3000/post/update?num=1 (to delete first post)
+URL - localhost:3000/post/delete?num=1 (to delete first post)
 
 UPDATE POSTS BY ID
 URL - localhost:3000/post/update?num=2 (to update second post)
 {
     "title": "Post one",
     "description": "Updated to first blog post!",
-    "authorId": 1
+    "authorName": "Michael Clark"
 }
 
 FETCH ALL POSTS BY A AUTHOR BY ITS AUTHORID
 URL - localhost:3000/posts/authorId?num=2 (to get all the posts from author id equals to 2)
-
